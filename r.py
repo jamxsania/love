4f1e5d3f63b27a1c49720d2cf52ed88a89cc37e5
jamxsania <92490461+jamxsania@users.noreply.github.com>
2021-10-17 06:51:06 -0700

    Update r.py

--- r.py
+++ r.py
@@ -1,10 +1,11 @@
 # Python bytecode 2.7
 # NAM TU SUNA HUGA
 # [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
-import os.path
-import requests
-from bs4 import BeautifulSoup
-import sys
+import requests, bs4, sys, os, subprocess, requests, sys, random
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from multiprocessing.pool import ThreadPool
+import requests, subprocess, logging
 
 def clear():
     if ' linux' in sys.platform.lower():
